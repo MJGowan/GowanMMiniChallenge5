aaf7879fd90329ba6f6ecb5c06ae4fbf061d9f3d
Madeline <116022163+MJGowan@users.noreply.github.com>
2022-10-20 15:08:13 -0700

    Add files via upload

--- GowanMMiniChallenge5/Program.cs
+++ GowanMMiniChallenge5/Program.cs
@@ -1,7 +1,28 @@
 ﻿//Maddie Gowan
 //10-18-2022
-//Mini Challenge #4 - Mad Lib
-//This is a redo of an old assignment
+//Mini Challenge #4 - Redo
+//In this program we have to create a MadLib, allowing the user to input various
+//words which will then combine together cleanly into a story.
+
+//Review by Daniel Decoito: Everything works as intended but there are a few issues.
+//First I felt that the user experience is not taken into consideration. It works as a
+//project because it meets the base criteria but more could have been implemented to
+//make the user more aware of what theyre supposed to do. Perhaps explain to them the
+//rules of madlibs and inform them that they are supposed to type in responses to
+//specific console prompts. Second, nothing was added to improve the base code since
+//the initial assignment was given. A good suggestion would be using string
+//interplotion to make the code cleaner. Finally, the loop mechanisim has issues.
+//If you put in an incorrect Character you will be prompted to renter the character
+//correctly. However, whatever the user inputs, correct or incorrect, will cause the
+//game to terminate. I think the issue is due to the if statment that triggers the
+//terminate game being outside the while loop.
+
+//Response by Madeline Gowan: I did my best to take this advice and make my program
+//better in terms of user experience, by explaining the code and the program as well
+//as adding friendlier responses. I adjusted the if statements and while loop to
+//hopefully fix the issue with the game being terminated. I also used string
+//interpolation to clean up the code, as suggested. Thank you very much for the review! It helped a lot.
+
 
 Console.Clear();
 
